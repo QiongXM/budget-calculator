4c3852e7023765c3b3bd1d94f6f3cf930eeab6e8
Cobunny <malatanggm@gmail.com>
2018-04-25 20:16:42 -0700

    Change form focus border-color accordingly by inc or exp type

--- app.js
+++ app.js
@@ -152,6 +152,13 @@ var UIController = (function() {
 
     return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec;
   };
+
+  var nodeListForEach = function(list, callback) {
+    for (var i = 0; i < list.length; i++) {
+      callback(list[i], i);
+    }
+  };
+
   return {
     getinput: function() {
       return {
@@ -230,12 +237,6 @@ var UIController = (function() {
     displayPercentages: function(percentages) {
       var fields = document.querySelectorAll(DOMstrings.expensesPerLabel);
 
-      var nodeListForEach = function(list, callback) {
-        for (var i = 0; i < list.length; i++) {
-          callback(list[i], i);
-        }
-      };
-
       nodeListForEach(fields, function(cur, index) {
         if (percentages[index] > 0) {
           cur.textContent = percentages[index] + '%';
@@ -266,6 +267,21 @@ var UIController = (function() {
       document.querySelector(DOMstrings.dateLabel).textContent =
         months[month] + ' ' + year;
     },
+    changedType: function() {
+      var fields = document.querySelectorAll(
+        DOMstrings.inputType +
+          ',' +
+          DOMstrings.inputDescription +
+          ',' +
+          DOMstrings.inputValue
+      );
+
+      nodeListForEach(fields, function(cur) {
+        cur.classList.toggle('red-focus');
+      });
+
+      document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
+    },
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -286,6 +302,10 @@ var controller = (function(budgetCtrl, UIContrl) {
     document
       .querySelector(DOM.container)
       .addEventListener('click', ctrlDeleteItem);
+
+    document
+      .querySelector(DOM.inputType)
+      .addEventListener('change', UIContrl.changedType);
   };
 
   var updateBudget = function() {
